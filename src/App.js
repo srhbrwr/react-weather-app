1c1726e4de04a68c18e88ffa35aaf455ce4f69bd
Sarah B <73038204+srhbrwr@users.noreply.github.com>
2022-11-14 09:56:05 -0600

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Sarah and is{" "}
-        <a href="https://github.com/srhbrwr/react-weather-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a 
+          href="https://sarahbarelos.com/" 
+          target="_blank" 
+          rel="noreferrer"
+          >
+            Sarah
+            </a> and is{" "}
+          <a
+            href="https://github.com/srhbrwr/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
