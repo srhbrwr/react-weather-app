0fca25fccf53b1f4dca317a8877538cf741c4001
Sarah B <73038204+srhbrwr@users.noreply.github.com>
2022-11-14 09:29:39 -0600

    Added GitHub Repo URL

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Sarah and is{" "}
+        <a href="https://github.com/srhbrwr/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
