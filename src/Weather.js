395e8ad54976b250c065e4ee444f3dd39743808a
Sarah B <73038204+srhbrwr@users.noreply.github.com>
2022-11-14 12:11:10 -0600

    installed axios and states

--- src/Weather.js
+++ src/Weather.js
@@ -1,53 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
-import "./index.css";
 
-export default function Weather() {
-    return (
-      <div className="Weather">
-        <form>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city.."
-                className="form-control"
-                autoFocus="on"
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
-              />
-            </div>
-          </div>
-        </form>
-        <h1>Omaha</h1>
-        <ul>
-          <li>Monday 10:15</li>
-          <li>Mostly Cloudy</li>
-        </ul>
-        <div className="row mt-3">
-          <div className="col-6 mt-3">
-            <span className="temperature">-2</span>
-            <span className="unit">°C</span>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>
-                <img
-                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                  alt="mostly cloudy"
+
+export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({ ready: false });
+
+    function handleResponse(response) {
+        console.log(response.data);
+        setWeatherData({
+          ready: true,
+          date: "Monday 10:15",
+          temperature: response.data.main.temp,
+          humidity: response.data.main.humidity,
+          description: response.data.weather[0].description,
+          icon: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+          wind: response.data.wind.speed,
+          city: response.data.name,
+        });
+
+    }
+
+    if (weatherData.ready) {
+      return (
+        <div className="Weather">
+          <form>
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Enter a city.."
+                  className="form-control"
+                  autoFocus="on"
                 />
-              </li>
-              <li>Precipitation: 24%</li>
-              <li>Humidity: 78%</li>
-              <li>Wind: 11 km/h</li>
-            </ul>
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-primary w-100"
+                />
+              </div>
+            </div>
+          </form>
+          <h1>{weatherData.city}</h1>
+          <ul>
+            <li>{weatherData.date}</li>
+            <li className="text-capitalize">{weatherData.description}</li>
+          </ul>
+          <div className="row mt-3">
+            <div className="col-6 mt-3">
+              <span className="temperature">{Math.round(weatherData.temperature)}</span>
+              <span className="unit">°C</span>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>
+                  <img
+                    src={weatherData.icon}
+                    alt={weatherData.description}
+                  />
+                </li>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {weatherData.wind} km/h</li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+    const apiKey = "6ccd6b17755ed4a6ceae6fef6e33efe5";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading..."
+    }
+
 }
+
+    
+
+
+    
