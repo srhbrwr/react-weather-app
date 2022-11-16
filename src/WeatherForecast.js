791cd27e2319c3f835808ec7afdee8280ab2177a
Sarah B <73038204+srhbrwr@users.noreply.github.com>
2022-11-16 11:59:21 -0600

    fixes

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,6 +17,15 @@ export default function WeatherForecast(props) {
         setLoaded(true);
     }
 
+    function load() {
+      let apiKey = "6ccd6b17755ed4a6ceae6fef6e33efe5";
+      let longitude = props.coordinates.lon;
+      let latitude = props.coordinates.lat;
+      let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+      axios.get(apiUrl).then(handleResponse);
+    }
+
     if (loaded) {
         return (
           <div className="WeatherForecast">
@@ -28,19 +37,16 @@ export default function WeatherForecast(props) {
                       <WeatherForecastDay data={dailyForecast} />
                     </div>
                   );
+                } else {
+                  return null;
                 }
               })}
             </div>
           </div>
         );
     } else {
-    let apiKey = "6ccd6b17755ed4a6ceae6fef6e33efe5";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
-
-    return null;
+      load();
+  
+      return null;
     }
 }
